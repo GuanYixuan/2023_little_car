c64ea9de87c5e1526fc29e29e3d4e88a38418401
gary318 <gary318@126.com>
2023-08-18 21:51:32 +0800

    Implement block position estimation

--- navigation/navigation.py
+++ navigation/navigation.py
@@ -19,10 +19,15 @@ os.chdir(os.path.dirname(__file__))
 from numpy.typing import NDArray
 from typing import List, Tuple
 
+DEBUG: bool = True
+
 TRANSFORMED_WIDTH: int = 1500
 TRANSFORMED_HEIGHT: int = 1000
 GAUSS_BLUR_KSIZE: int = 7
 
+BLOCK_HSV_LOWERBOUND: Tuple[int, int, int] = (17, 128, 128)
+BLOCK_HSV_UPPERBOUND: Tuple[int, int ,int] = (33, 255, 255)
+BLOCK_SIZE_THRESH: int = 0
 
 class Navigator:
     """定位导航主类"""
@@ -53,18 +58,52 @@ class Navigator:
         cv2.setMouseCallback("select_corner", self.__tag_corner_callback, param=corner_list)
         cv2.waitKey(-1)
 
+        assert DEBUG
+        corner_list = [(354, 1014), (1443, 943), (1308, 458), (557, 565)]
+
         assert len(corner_list) == 4
         self.transform_to_top, _unused = cv2.findHomography(np.array(corner_list), np.array([(0, TRANSFORMED_HEIGHT), (TRANSFORMED_WIDTH, TRANSFORMED_HEIGHT), (TRANSFORMED_WIDTH, 0), (0, 0)]))
+
+        self.refresh_image()
+        self.estimate_block_pose()
 
     def __tag_corner_callback(self, event: int, x: int, y: int, flags: int, corner_list: List[Tuple[int, int]]) -> None:
         """生成变换矩阵时的callback函数"""
         if event == cv2.EVENT_LBUTTONUP:
             corner_list.append((x, y))
 
+    def transform_points(self, input_points: "Tuple | np.ndarray", round: bool = False) -> np.ndarray:
+        """对一个或一系列点坐标施加变换
+
+        Args:
+            input_points (Arraylike): 将要变换的点坐标, 可以是一维数组或二维数组
+            round (bool, optional): 是否对结果取整, 默认为否.
+
+        Returns:
+            np.ndarray: 经过变换后的点坐标, 原则上与input_points的shape一致
+        """
+        input_points = np.array(input_points)
+        one_dim: bool = len(input_points.shape) == 1
+
+        if one_dim:
+            input_points = np.array([*input_points, 1]).reshape((3, 1)) # 转化到 (3, 1)
+        else:
+            input_points = np.pad(input_points, ((0, 0), (0, 1)), 'constant', constant_values=1).T # 转化到 shape (3, N)
+
+        product = np.dot(self.transform_to_top, input_points).T # (3, 3) * (3, N) = (3, N) -> (N, 3)
+        product = product[:, :2] / product[:, 2]
+        if one_dim:
+            product = product[0]
+
+        if round:
+            return np.round(product).astype(np.int32)
+        else:
+            return product
+
     def refresh_image(self, init: bool = False) -> None:
         """阻塞式地刷新图片"""
 
-        self.image_rgb = cv2.imread("1692349092147.jpg") # 造点假
+        self.image_rgb = cv2.imread("test_images/desk_high.jpg") # 造点假
         self.image_rgb = cv2.resize(self.image_rgb, (1920, 1080))
 
         if init:
@@ -74,17 +113,30 @@ class Navigator:
         self.transformed_rgb = cv2.warpPerspective(self.image_rgb, self.transform_to_top, dsize=(TRANSFORMED_WIDTH, TRANSFORMED_HEIGHT))
 
     def estimate_block_pose(self) -> None:
+        # 模糊后进行颜色筛选
         blurred = cv2.cvtColor(cv2.GaussianBlur(self.image_rgb, (GAUSS_BLUR_KSIZE*3, GAUSS_BLUR_KSIZE*3), GAUSS_BLUR_KSIZE), cv2.COLOR_BGR2HSV)
+        block_mask = cv2.inRange(blurred, BLOCK_HSV_LOWERBOUND, BLOCK_HSV_UPPERBOUND) # type: ignore
 
-        进行颜色过滤及色块提取
+        # 分离轮廓
+        raw_contours, _unused = cv2.findContours(block_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-        按大小筛选色块
+        temp_output = np.copy(self.image_rgb)
+        for ind, contour in enumerate(raw_contours):
+            if cv2.contourArea(contour) < BLOCK_SIZE_THRESH:
+                continue
 
-        for 色块 in 色块s:
+            # 取最靠屏幕下方的像素位置
+            contour_img = cv2.drawContours(np.zeros(self.image_rgb.shape[:-1]), raw_contours, ind, (1,), -1)
+            pixels = np.where(contour_img == 1)
+            bottom_ind = np.argmax(pixels[0])
+            bottom_position = (pixels[1][bottom_ind], pixels[0][bottom_ind])
 
-            获取色块中心
+            cv2.circle(temp_output, bottom_position, 5, (0, 255, 0), -1)
+            cv2.putText(temp_output, str(ind), bottom_position, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0))
+            cv2.circle(self.transformed_rgb, self.transform_points(bottom_position, True), 5, (0, 255, 0), -1)
 
-            利用仿射变换矩阵获取物体在场地中的坐标
+        plt.imshow(self.transformed_rgb)
+        plt.show(block=True)
 
     def estimate_car_pose(self) -> None:
         """根据图片更新小车位姿"""
